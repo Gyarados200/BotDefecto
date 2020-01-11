8db70d3ba7b972397974d9bd16bdf7604b87374a
Gyarados200 <49349558+Gyarados200@users.noreply.github.com>
2020-01-11 18:46:36 -0500

    Update config-example.js

--- config-example.js
+++ config-example.js
@@ -1,6 +1,6 @@
 /* Websocket server and port the bot should connect to. Check the 'Request URL'
  * of the websocket. If you don't know what that means, just leave this as-is. */
-exports.server = "sim3.psim.us";
+exports.server = "pokemonhispano.psim.us";
 exports.port = 80;
 
 /* The server ID. This should almost certainly be "showdown", but just in case,
